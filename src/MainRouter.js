c02073145c8328c91061ee8fe57a1b62fe23ecd3
NaveenDK <naveenkolambage@gmail.com>
2021-02-07 21:33:10 +1300

    added signing in functionality, request token

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {Route,Switch } from 'react-router-dom'
 import Home from './core/Home';
 import Signup from './User/Signup';
+import Signin from './User/Signin';
 
 
 const MainRouter = () =>(
     <div>
         <Switch>
-        <Route path="/signup" component={Signup}>
+        <Route exact path="/" component={Home}>
         </Route>
-        
-        <Route path="/" component={Home}>
+        <Route exact path="/signup" component={Signup}>
+        </Route>
+        <Route exact path="/signin" component={Signin}>
         </Route>
                
         </Switch>
