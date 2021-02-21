961ab651cc9c78535033e9eacccae3372b0d29ff
NaveenDK <naveenkolambage@gmail.com>
2021-02-21 23:41:30 +1300

    deleteuser

--- src/User/DeleteUser.js
+++ src/User/DeleteUser.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
-
+import {isLoggedIn} from '../auth'
 
 class DeleteUser extends Component{
+
+
+deleteAccount = ()=>{
+    
+    const token = isLoggedIn().token
+    const userId = this.props.userId
+    remove(userId,token)
+}
+
+    
+deleteConfirmed = () => {
+    let answer = window.confirm(
+        "Are you sure you want to delete your account?"
+    )
+    if(answer){
+        this.deleteAccount()
+    }
+}
+
+
     render(){
         return(
-            <button className="btn btn-raised btn-danger">
+            <button onClick={this.deleteConfirmed} className="btn btn-raised btn-danger">
                  Delete Profile
             </button>
         )
